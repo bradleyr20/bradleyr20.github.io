8b42159032340cc942210f1650993d8c74f62f52
bradleyr20 <bradleyrkmn@gmail.com>
2023-03-21 16:06:45 -0400

    Updates to styles, scripts, and new pages

--- wireframes/main.js
+++ wireframes/main.js
@@ -9,3 +9,49 @@ function showHide() {
 
 document.querySelector("#hamburger").addEventListener("click", showHide);
 document.querySelector(".closeX").addEventListener("click", showHide);
+
+
+
+// restaurant list sorting script
+function sortRestList() {
+  // Declare variables
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById('restSelect');
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("restaurantSorts");
+  li = ul.getElementsByTagName('li');
+
+  // Loop through all list items, and hide those who don't match the search query
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("a")[0];
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
+
+
+// Function to filter Restaurant list by name, using input box
+function selectRest() {
+  // Declare variables
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById('restSelect');
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("RestaurantList");
+  li = ul.getElementsByTagName('li');
+
+  // Loop through all list items, and hide those who don't match the search query
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("a")[0];
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
+
