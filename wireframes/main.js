672e7cb1ae27ae403526f894c9888769adda552e
bradleyr20 <bradleyrkmn@gmail.com>
2023-04-07 03:21:31 -0400

    Lots of refactoring and new nav work

--- wireframes/main.js
+++ wireframes/main.js
@@ -1,35 +1,67 @@
-document.getElementById("hamburger").addEventListener("click", showHide);
-document.getElementById("closeBurger").addEventListener("click", showHide);
+const menu = document.getElementById("siteMenu"), 
+  hamburger = document.getElementById("hamburger"),
+  closeBurger = document.getElementById("closeBurger"),
+  showSubs = document.getElementsByClassName("subNavParent"),
+  subMenus = document.getElementsByClassName("subNavGroup");
 
-function showHide() {
-  var x = document.getElementById("siteMenu");
-  if (x.style.display == "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
+hamburger.addEventListener("click", () => {
+  menu.style.display = "block";
+});
+
+closeBurger.addEventListener("click", () => {
+  menu.style.display = "none";
+});
+
+// Accordion Nav script that
+// lets just one subNav be open at a time
+for (let i = 0; i < showSubs.length; i++) {
+  showSubs[i].addEventListener("click", () => {
+      for (let j = 0; j < subMenus.length; j++) {
+          if (i == j) {
+            subMenus[j].style.display = subMenus[j].style.display == "block" ? "none" : "block";
+          } else {
+            subMenus[j].style.display = "none";
+          }
+      }
+  });
 }
+
+function makeHeader() {
+  fetch("header.html")
+  .then(response => {
+    return response.text()
+  })
+  .then(data => {
+    document.getElementById("header").innerHTML = data;
+  });
+}
+
+makeHeader();
+
+
+
+
 
 // restaurant list sorting script
-function sortRestList() {
-  // Declare variables
-  var input, filter, ul, li, a, i, txtValue;
-  input = document.getElementById('restSelect');
-  filter = input.value.toUpperCase();
-  ul = document.getElementById("restaurantSorts");
-  li = ul.getElementsByTagName('li');
+// function sortRestList() {
+//   // Declare variables
+//   var input, filter, ul, li, a, i, txtValue;
+//   input = document.getElementById('restSelect');
+//   filter = input.value.toUpperCase();
+//   ul = document.getElementById("restaurantSorts");
+//   li = ul.getElementsByTagName('li');
 
-  // Loop through all list items, and hide those who don't match the search query
-  for (i = 0; i < li.length; i++) {
-    a = li[i].getElementsByTagName("a")[0];
-    txtValue = a.textContent || a.innerText;
-    if (txtValue.toUpperCase().indexOf(filter) > -1) {
-      li[i].style.display = "";
-    } else {
-      li[i].style.display = "none";
-    }
-  }
-}
+//   // Loop through all list items, and hide those who don't match the search query
+//   for (i = 0; i < li.length; i++) {
+//     a = li[i].getElementsByTagName("a")[0];
+//     txtValue = a.textContent || a.innerText;
+//     if (txtValue.toUpperCase().indexOf(filter) > -1) {
+//       li[i].style.display = "";
+//     } else {
+//       li[i].style.display = "none";
+//     }
+//   }
+// }
 
 
 // Function to filter Restaurant list by name, using input box
